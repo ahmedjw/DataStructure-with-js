6a34cf9e6276c885fdb4777636961d00e14c33ac
ahmedjw <ahmedvander@gmail.com>
2022-07-04 17:00:16 +0300

    initialize a folder for problems related to linkedList from leetcode

--- Single linked list/sum2linkedlist2.js
+++ Single linked list/sum2linkedlist2.js
@@ -5,13 +5,23 @@ function ListNode(val, next) {
 
 var addTwoNumbers = function (l1, l2) {
   let linkedList = null;
+  carry = 0;
   while (l1 != undefined || l2 != undefined) {
     linkedList = {
-      val: l2 === undefined ? l1 : l1 === undefined ? l2 : l2.val + l1.val,
+      val:
+        l2 === undefined
+          ? l1 + carry
+          : l1 === undefined
+          ? l2 + carry
+          : l2.val + l1.val >= 10
+          ? 0
+          : l2.val + l1.val + carry,
       next: linkedList,
     };
+    linkedList.val === 0 ? carry++ : "";
     l1 = l1.next;
     l2 = l2.next;
   }
+
   return linkedList;
 };
